6ce90d757c3726f7d00855d599f4309d0a36bff8
daycry <7590335+daycry@users.noreply.github.com>
2023-11-23 15:13:28 +0100

    exceptions

---
+++ src/Exceptions/RuntimeException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daycry\Exceptions\Exceptions;
+
+use Daycry\Exceptions\Interfaces\BaseException;
+
+class RuntimeException extends \RuntimeException implements BaseException
+{
+}
---
+++ src/Interfaces/BaseExceptionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Daycry\Exceptions\Interfaces;
+
+use Throwable;
+
+/**
+ * Base Exception Interface for RestFul
+ */
+interface BaseException extends Throwable
+{
+}
