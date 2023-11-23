1afc4ac6daac882275530dafe8a51e362a9fa1f7
daycry <7590335+daycry@users.noreply.github.com>
2023-11-23 15:30:43 +0100

    exceptions

--- src/Exceptions/RuntimeException.php
+++ src/Exceptions/RuntimeException.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Daycry\Exceptions\Exceptions;
 
-use Daycry\Exceptions\Interfaces\BaseException;
+use Daycry\Exceptions\Interfaces\BaseExceptionInterface;
 
-class RuntimeException extends \RuntimeException implements BaseException
+class RuntimeException extends \RuntimeException implements BaseExceptionInterface
 {
 }
